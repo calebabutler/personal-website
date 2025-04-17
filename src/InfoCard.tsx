015a9ddf99810b2966d0899f818653bb7eb5eda5
Caleb Butler <caleb@calebbutler.dev>
2025-04-16 23:39:38 -0400

    Fix formatting

--- src/InfoCard.tsx
+++ src/InfoCard.tsx
@@ -2,18 +2,18 @@ import { ReactNode } from "react";
 import { Card } from "react-bootstrap";
 
 const InfoCard = (): ReactNode => {
-    return     <Card style={{ width: '18rem' }} className="border-0">
-    <Card.Body>
-        <div className="ratio ratio-1x1 rounded-circle overflow-hidden">
-            <Card.Img variant="top" src="profile.png" />
-        </div>
-        <br />
-      <Card.Title>Caleb Butler</Card.Title>
-      <Card.Text>
-        A little about who I am.
-      </Card.Text>
-    </Card.Body>
-  </Card>;
-}
+    return (
+        <Card style={{ width: "18rem" }} className="border-0">
+            <Card.Body>
+                <div className="ratio ratio-1x1 rounded-circle overflow-hidden">
+                    <Card.Img variant="top" src="profile.png" />
+                </div>
+                <br />
+                <Card.Title>Caleb Butler</Card.Title>
+                <Card.Text>A little about who I am.</Card.Text>
+            </Card.Body>
+        </Card>
+    );
+};
 
 export default InfoCard;
