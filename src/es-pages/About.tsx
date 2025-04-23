8cd0bd05418e22eb88d6d88c3b292d8c71d1df70
Caleb Butler <caleb@calebbutler.dev>
2025-04-23 16:16:17 -0400

    Finish about page and basic RSS

--- src/es-pages/About.tsx
+++ src/es-pages/About.tsx
@@ -1,7 +1,45 @@
 import { ReactNode } from "react";
+import { Card, Col, Row } from "react-bootstrap";
 
 const About = (): ReactNode => {
-    return <>About.</>;
+    return (
+        <>
+            <p>Vivo con mi prometida, Carla, y dos gatos, Blue y Walnut.</p>
+            <p>
+                A Blue le encanta dormir y comer Meow Mix. También le encantan
+                los aperitivos Fancy Feast. Es muy hablador y le gusta sacar la
+                lengua.
+            </p>
+            <p>
+                A Walnut le encanta saltar sobre la gente cuando menos se lo
+                esperan. Le gusta que la carguen y la acaricien, y te arañará si
+                no la acaricias. Come pienso seco. Es muy juguetona.
+            </p>
+            <p>A ambos les encantan las golosinas para gatos Temptations.</p>
+            <Row xs={2} className="g-4">
+                <Col>
+                    <Card className="h-100 border-0 text-center">
+                        <Card.Img src="blue.jpg" />
+                        <Card.Body>
+                            <Card.Text>
+                                <i>Blue</i>
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card className="h-100 border-0 text-center">
+                        <Card.Img src="walnut.jpg" />
+                        <Card.Body>
+                            <Card.Text>
+                                <i>Walnut</i>
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+        </>
+    );
 };
 
 export default About;
